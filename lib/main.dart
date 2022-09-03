00be6b08dd9f56214124399bb34e96c789b35236
Liroy Barzillai <barzillai.liroy@gmail.com>
2022-09-03 21:51:42 +0300

    Added image asset

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,8 @@ void main() {
             backgroundColor: Colors.blueGrey[900]),
         backgroundColor: Colors.blueGrey,
         body: const Center(
-          child: const Image(
-            image: NetworkImage(
-                'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752'),
+          child: Image(
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
